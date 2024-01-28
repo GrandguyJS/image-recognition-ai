0a5dc769d943e70ac24a584f445a5dbd8a4e71d7
Timo Ihbe <timo@MBP-von-Timo.fritz.box>
2024-01-28 21:58:25 +0100

    Finished neural network base structure. Added NeuralNetwork class and fixed errors in the Level class

--- Code/utils.py
+++ Code/utils.py
@@ -1,1 +1,6 @@
 # Utility function
+
+# Takes a number A and B and returns the middle value of both by the factor t
+# For example if A is 10, B is 20 and t is 0.5 it would return 15
+def lerp(A, B, t):
+    return A+(B-A)*t
