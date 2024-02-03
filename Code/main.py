3e7958fa71bd919d234062f3eb93159c0954a97d
GrandguyJS <GrandguyMC@gmail.com>
2024-02-03 09:23:52 +0100

    final touches, AI works for cats and dogs with insane accuracy

--- Code/main.py
+++ Code/main.py
@@ -1,21 +1,23 @@
 # Run, train and test the photos in the neural network
 
 from neuralNetwork import NeuralNetwork, Level
-from imgConverter import get_image_batch, resize_image
+from imgConverter import get_image_batch, resize_image, load_all_images
 import utils
 import numpy as np
 import pickle
 import os
 
 # Set Neural Network layers
-layers = [40000, 100, 100, 2]
+layers = [40000, 128, 1]
 # How many photos to input at once into the neural network
-batch_size = 32
-
+batch_size = 100
+# Set the neuralnetwork path
+network_path = "../Network/network.pkl"
 # Load the neuralnetwork from the filename
-nn = utils.loadFormerNetwork("network.pkl")
-
-train = True
+nn = utils.loadFormerNetwork(network_path)
+# Set to False if you want to test, elso to True
+train = False
+generation = 0
 if train:
     if nn is not None:
         # If the neralnetwork exists do nothing
@@ -26,33 +28,30 @@ if train:
 
     # Set loss to 1 and run the neuralnetwork as long as oss is bigger than x, in this case 0.003
     loss = 1
-    generation = 1
-    ngenerations = 20
+    # Set the best loss this will get better by time
     best = 1
+    # Load all images so you can save yourself a lot of time
+    load_all_images()
+    try:
+        for i in range(0, 10000):
+            # Keep track of generation
+            generation += 1
+            # Get new random images and labels
+            images, labels = get_image_batch(batch_size)
+            # Get the loss
+            loss = NeuralNetwork.train(nn, images, labels, 0.1)
+            # Print new best
+            print(f"Generation: {str(generation)}, Loss: {str(loss)}")
 
-    while loss > 0.003:
-        # Get new random images and labels
-        images, labels = get_image_batch(batch_size)
-        # Get the loss
-        loss = NeuralNetwork.train(nn, images, labels, 0.1)
-        # Print new best
-        try:
             if loss < best:
-                print("-- NEW BEST --")
-                print("Loss: " + str(loss))
                 best = loss
-
-
-        
-
+    finally:
+        utils.save_object(nn, network_path)
+        print("Saved NeuralNetwork under " + network_path)
     # After all, save the neural network
-    utils.save_object(nn, "network.pkl")
+    
 else:
-    path = "/Users/grandguymc/Downloads/Cat:Dog/train/dog.4277.jpg"
-    img = resize_image(path)
-    labelcars = path.split("/")[-1][:3]
-    label = np.array([[0, 1]]) 
-    if labelcars == "cat":
-        label = np.array([[1, 0]]) 
-    output = NeuralNetwork.feedForward(nn, img)
-    print(np.mean(output - label))
+    # Test code
+    images, labels = get_image_batch(batch_size, False)
+    accuracy = NeuralNetwork.test(nn, images, labels)
+    print("Accuracy: " + str(accuracy))
